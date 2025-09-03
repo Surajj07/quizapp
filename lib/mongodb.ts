53040b81e657ac0b7a9410a6141063e70465fa0a
suraj <sthakur@gmail.com>
2025-09-04 01:33:18 +0530

    bug fix

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,8 +1,22 @@
 import mongoose from "mongoose";
 
+const MONGO_URI = process.env.MONGO_URI as string;
+
+if (!MONGO_URI) {
+  throw new Error("Please add your Mongo URI to .env");
+}
+
+let isConnected = false;
+
 export async function connectDB() {
-  console.log("MONGO_URI", process.env.MONGO_URI);
-  if (!process.env.MONGO_URI) throw new Error("MONGO_URI not set in env");
-  if (mongoose.connection.readyState >= 1) return;
-  await mongoose.connect(process.env.MONGO_URI);
+  if (isConnected) return;
+
+  try {
+    await mongoose.connect(MONGO_URI);
+    isConnected = true;
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+    throw err;
+  }
 }
